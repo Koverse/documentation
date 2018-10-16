66e898efbbc54e4894fe1e4fc380253a2733ca3f
Tyler Porter <porteta@gmail.com>
2018-10-16 14:50:10 -0700

    add material ui packages

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,67 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Router, Link } from 'react-static'
 import { hot } from 'react-hot-loader'
 //
 import Routes from 'react-static-routes'
 
-import './app.css'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import { withStyles } from '@material-ui/core/styles'
 
-const App = () => (
-  <Router>
-    <div>
-      <nav>
-        <Link exact to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-      </nav>
-      <div className="content">
-        <Routes />
-      </div>
-    </div>
-  </Router>
-)
+// Custom styles
+const styles = {
+  '@global': {
+    img: {
+      maxWidth: '100%',
+    },
+  },
+  appBar: {
+    flexWrap: 'wrap',
+  },
+  tabs: {
+    width: '100%',
+  },
+  content: {
+    padding: '1rem',
+  },
+}
 
-export default hot(module)(App)
+class App extends PureComponent {
+  // Remove the server-side injected CSS.
+  componentDidMount () {
+    const jssStyles = document.getElementById('jss-server-side')
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }
+
+  render () {
+    const { classes } = this.props
+
+    return (
+      <Router>
+        <div className={classes.container}>
+          <CssBaseline />
+          <AppBar className={classes.appBar} color="default" position="static">
+            <nav>
+              <Tabs className={classes.tabs} value={false}>
+                <Tab component={Link} to="/" label="Home" />
+                <Tab component={Link} to="/about" label="About" />
+                <Tab component={Link} to="/blog" label="Blog" />
+              </Tabs>
+            </nav>
+          </AppBar>
+          <div className={classes.content}>
+            <Routes />
+          </div>
+        </div>
+      </Router>
+    )
+  }
+}
+
+const AppWithStyles = withStyles(styles)(App)
+
+export default hot(module)(AppWithStyles)
--- app/static.config.js
+++ app/static.config.js
@@ -1,4 +1,11 @@
 import axios from 'axios'
+import React, { Component } from 'react'
+import { SheetsRegistry } from 'react-jss/lib/jss'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from '@material-ui/core/styles'
+
+// Your Material UI Custom theme
+import theme from './src/theme'
 
 export default {
   getSiteData: () => ({
@@ -34,5 +41,50 @@ export default {
         component: 'src/containers/404',
       },
     ]
+  },
+  renderToHtml: (render, Comp, meta) => {
+    // Create a sheetsRegistry instance.
+    const sheetsRegistry = new SheetsRegistry()
+
+    // Create a MUI theme instance.
+    const muiTheme = createMuiTheme(theme)
+
+    const generateClassName = createGenerateClassName()
+
+    const html = render(
+      <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+        <MuiThemeProvider theme={muiTheme} sheetsManager={new Map()}>
+          <Comp />
+        </MuiThemeProvider>
+      </JssProvider>
+    )
+
+    meta.jssStyles = sheetsRegistry.toString()
+
+    return html
+  },
+  Document: class CustomHtml extends Component {
+    render () {
+      const {
+        Html, Head, Body, children, renderMeta,
+      } = this.props
+
+      return (
+        <Html>
+          <Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link
+              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+              rel="stylesheet"
+            />
+          </Head>
+          <Body>
+            {children}
+            <style id="jss-server-side">{renderMeta.jssStyles}</style>
+          </Body>
+        </Html>
+      )
+    }
   },
 }
