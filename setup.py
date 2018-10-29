babc1d283bf177d95691db61898e2df1d61a16ab
Ian Barker <ianbarker@Ians-MacBook-Pro-2.local>
2018-10-29 14:34:01 -0700

    Removing setuptools as it was a dead end.

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ setup(
     author_email='support@koverse.com',
     packages=find_packages(),
     description='Koverse Documentation',
-    setup_requires=[
-        'setuptools_scm',
-    ],
     install_requires=[
       'sphinxcontrib-httpdomain >= 1.5.0',
       'sphinxcontrib-openapi',
