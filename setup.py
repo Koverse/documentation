cdba578df0b4c80236592d9536ae5f9f2ec4783e
Ian Barker <ianbarker@Ians-MacBook-Pro-2.local>
2018-10-29 14:03:43 -0700

    Adding setuptools to setup to try and get virtual environment working on RTD.

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(
     author_email='support@koverse.com',
     packages=find_packages(),
     description='Koverse Documentation',
+    setup_requires=[
+        'setuptools_scm',
+    ]
     install_requires=[
       'sphinxcontrib-httpdomain >= 1.5.0',
       'sphinxcontrib-openapi',
